c0538ace170a6593d2181b3083494becff47aa69
Zverev-M <mishazve@gmail.com>
2021-05-18 19:59:35 +0300

    Const.py deleted Exception handling added Names of files with gaps in input file handled

--- FileHashChecker.py
+++ FileHashChecker.py
@@ -1,7 +1,7 @@
 import hashlib
-import Const
 
-func_list = {'mb5': hashlib.md5, 'sha1': hashlib.sha1, 'sha256': hashlib.sha256}
+BUF = 65536
+func_list = {'md5': hashlib.md5, 'sha1': hashlib.sha1, 'sha256': hashlib.sha256}
 
 
 def check(path, file_to_check, func_name, expected_hash_code):
@@ -9,7 +9,7 @@ def check(path, file_to_check, func_name, expected_hash_code):
     try:
         file = open(path + "\\" + file_to_check, 'rb')
         while True:
-            part = file.read(Const.BUF)
+            part = file.read(BUF)
             if not part:
                 break
             h.update(part)
@@ -20,6 +20,6 @@ def check(path, file_to_check, func_name, expected_hash_code):
             return file_to_check + " OK"
         else:
             return file_to_check + " FAIL"
-    except FileNotFoundError:
+    except FileNotFoundError as e:
         return file_to_check + " NOT FOUND"
 
--- Main.py
+++ Main.py
@@ -4,16 +4,17 @@ import FileHashChecker
 path_to_log_file = sys.argv[1]
 path_to_directory = sys.argv[2]
 
-file = open(path_to_log_file, 'r')
-data = file.readline().split()
-while data:
-    fileName = data[0]
-    funcName = data[1]
-    code = data[2]
-
-    print(FileHashChecker.check(path_to_directory, fileName, funcName, code))
+try:
+    file = open(path_to_log_file, 'r')
     data = file.readline().split()
+    while data:
+        file_name = ' '.join(data[0:len(data) - 2])
+        func_name = data[len(data) - 2]
+        code = data[len(data) - 1]
 
-file.close()
+        print(FileHashChecker.check(path_to_directory, file_name, func_name, code))
+        data = file.readline().split()
 
-
+    file.close()
+except IOError as e:
+    print("Can't open input file")
